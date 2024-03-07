b822c445590c632ad7f6dd406f8bc5a699b9acf4
KyleD <kyleandradebasquena@gmail.com>
2024-03-07 17:32:44 +0800

    added routing and pages

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-
 // Routes
 
 router.get('', (req, res) => {
-    res.send("Hello World");
+    const locals = {
+        title: "Kyle's NodeJs Blog",
+        description: "My first simple blog created with NodeJs, ExpressJs and MonggoDb."
+    }
+    res.render('index', { locals });
+    // res.render('index', locals); you can remove the curly brace if you're not passing multiple objects
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
 });
 
 
